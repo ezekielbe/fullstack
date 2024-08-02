2474f761059e2c7183c306aea7d157bdfa1175e4
ezekielbe <ezekielbeee@gmail,co>
2024-08-02 00:47:54 -0700

    Refactor components and resolve issues

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,24 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
+import { Link } from 'react-router-dom';
 
 const ItemList = ({ items, onItemClick }) => {
   return (
     <ul className="item-list">
       {items.map(item => (
         <li key={item._id} onClick={() => onItemClick(item)}>
-          <a href="#">{item.title}</a>
-=======
-import { Link } from 'react-router-dom';
-
-const ItemList = ({ items }) => {
-  return (
-    <ul className="item-list">
-      {items.map(item => (
-        <li key={item._id}>
           <Link to={`/items/${item._id}`}>
             {item.title}
           </Link>
->>>>>>> 91ac9a82 (Initial commit)
         </li>
       ))}
     </ul>
