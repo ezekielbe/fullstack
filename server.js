dd5198c8d8b8211284e46c3e78714ed7b316f50f
ezekielbe <ezekielbeee@gmail,co>
2024-08-01 22:41:36 -0700

    Updated server.js to serve React app and API

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./db');
 const itemRoutes = require('./routes/items'); // Ensure this path is correct
 
@@ -11,10 +12,19 @@ connectDB();
 // Enable CORS
 app.use(cors());
 
+// Middleware to parse JSON
 app.use(express.json());
 
-// Use item routes
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Use item routes for API
 app.use('/api/items', itemRoutes);
+
+// Catch-all route to serve React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5001;
 
